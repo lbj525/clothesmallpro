44059dfe39e06eb019f79bfeab226410038048d7
lbj525 <2995180490@qq.com>
2020-08-20 17:52:05 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,12 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    {{message}}
-    {{num}}
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  data() {
-    return {
-      message: "lbj",
-      num: 1,
-    };
-  },
-  components: {
-    HelloWorld,
-  },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
